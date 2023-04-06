f8a2297f75d2893c02ac577861da3e447d3a7246
Stanislav Osipkin <stan1976@mail.ru>
2023-04-06 17:04:03 +0500

    Practice 18.8 - changhed script.js

--- 18_grid/js/script.js
+++ 18_grid/js/script.js
@@ -1,16 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("shift__menu").addEventListener("click", function()
-  {
-    document.querySelector(".header").classList.toggle("open")
-  })
-})
-
 let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.header__link');
+let btn = document.getElementById('shift__menu');
+
+
+burger.addEventListener('click',
+
+  function () {
+
+    burger.classList.toggle('burger--active');
+
+    menu.classList.toggle('header__nav--active');
+
+  })
+
+
 burger.addEventListener('click', () => {
 
-  let n = 'true' === burger.getAttribute('aria-expanded');
-  burger.setAttribute('aria-expanded', !n),
-    n
-      ? burger.setAttribute('aria-label', 'Открыть навигацию')
-      : burger.setAttribute('aria-label', 'Закрыть навигацию');
+    let n = 'true' === burger.getAttribute('aria-expanded');
+    burger.setAttribute('aria-expanded', !n),
+      n
+        ? burger.setAttribute('aria-label', 'Открыть навигацию')
+        : burger.setAttribute('aria-label', 'Закрыть навигацию');
+  })
+
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+
+    burger.classList.remove('burger--active');
+
+    menu.classList.remove('header__nav--active');
+
+    btn.ariaLabel = 'Открыть меню навигации';
+  })
 })
